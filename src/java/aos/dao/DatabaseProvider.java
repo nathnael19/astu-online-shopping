796999c36bfb2184807aaf703fee7001917bf63a
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2024-12-28 21:33:43 +0300

    creating Database Provider class and admin's homepage

--- src/java/aos/dao/DatabaseProvider.java
+++ src/java/aos/dao/DatabaseProvider.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aos.dao;
 
-/**
- *
- * @author DELL
- */
+import java.sql.*;
+
 public class DatabaseProvider {
-    
+
+    static Connection connection = null;
+    static String url = "jdbc:mysql://localhost:3306/";
+    static String userName = "root";
+    static String password = "root";
+
+    public static Connection getConn() {
+        String createDatabase = "create database if not exists suq;";
+        String useDatabase = "use suq;";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, userName, password);
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(createDatabase);
+            stmt.executeUpdate(useDatabase);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
 }
